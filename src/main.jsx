5cf23f3b1168d7b8345cd64f7ad78912b60c0fe7
mazdakxezri <167007604+mazdakxezri@users.noreply.github.com>
2024-11-01 16:56:14 +0330

    Add files via upload

---
+++ src/main.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+
+import * as Sentry from "@sentry/react";
+
+Sentry.init({
+  dsn: "https://2eea3869385eceae76cacd37fc78a4c7@o4508200861564928.ingest.us.sentry.io/4508200865759232",
+  integrations: [
+    Sentry.browserTracingIntegration(),
+    // Sentry.metrics.metricsAggregatorIntegration(),
+    Sentry.reactRouterV6BrowserTracingIntegration({
+      useEffect: React.useEffect,
+    }),
+    Sentry.replayIntegration({
+      maskAllText: false,
+      blockAllMedia: false,
+    }),
+  ],
+
+  tracesSampleRate: 1.0,
+  tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
+
+  replaysSessionSampleRate: 0.1,
+  replaysOnErrorSampleRate: 1.0,
+});
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
