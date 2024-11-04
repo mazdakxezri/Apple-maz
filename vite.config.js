fc1f81fa160001ae5ed4587200444bd573f2c565
mazdakxezri <167007604+mazdakxezri@users.noreply.github.com>
2024-11-04 21:12:49 +0330

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/my-vite-app/', } ```[_{{{CITATION{{{_2{How to Host a Vite.js App on GitHub Pages](https://gist.github.com/Dev5G/ecb8551c60e2cb36d884be705c0ae23d),
   plugins: [react(), sentryVitePlugin({
     org: "jsm-d7u",
     project: "javascript-react"
